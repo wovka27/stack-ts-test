6d580b3a77ead63da6a399c47da951e1c107db49
Vladimir <wovka.pb@yandex.ru>
2025-10-25 19:32:07 +0500

    [stack-ts-test] chore: Add base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(() => ({
+export default defineConfig(({mode}) => ({
+  base: mode === 'production' ? '/stack-ts-test/' : '/',
   plugins: [tsconfigPaths()],
   resolve: {
     alias: {
